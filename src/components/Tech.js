3c1d0ac1c27f30e29937daaf2440677798d5907e
Jason Toups <me@jasontoups.com>
2021-11-30 15:49:11 -0800

    adds the tech stack component

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,11 +1,66 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faJs, faHtml5, faSass, faNodeJs, faEnvira, faNode } from '@fortawesome/free-brands-svg-icons';
 import React from 'react';
 
 const Tech = () => {
 
 	return (
-		<div className='portfolio-site'>
-			Tech
-		</div>
+		<section className='tech'>
+      <div className="heading">
+        <h1>Tech Stack</h1>
+        <h2>I work with these technologies:</h2>
+      </div>
+      <div className="tech-items--container">
+        <div className="tech-item">
+          <span className="service-icon">
+            <FontAwesomeIcon icon={faReact} />
+          </span>
+          <p>
+            React, Redux & Hooks
+          </p>
+        </div>
+        <div className="tech-item">
+          <span className="service-icon">
+            <FontAwesomeIcon icon={faJs} />
+          </span>
+          <p>
+            Javascript ES6
+          </p>
+        </div>
+        <div className="tech-item">
+          <span className="service-icon">
+            <FontAwesomeIcon icon={faHtml5} />
+          </span>
+          <p>
+            HTML 5
+          </p>
+        </div>
+        <div className="tech-item">
+          <span className="service-icon">
+            <FontAwesomeIcon icon={faSass} />
+          </span>
+          <p>
+            CSS 3
+          </p>
+        </div>
+        <div className="tech-item">
+          <span className="service-icon">
+            <FontAwesomeIcon icon={faNodeJs} />
+          </span>
+          <p>
+            Node JS
+          </p>
+        </div>
+        <div className="tech-item">
+          <span className="service-icon">
+            <FontAwesomeIcon icon={faEnvira} />
+          </span>
+          <p>
+            MongoDB
+          </p>
+        </div>
+      </div>
+    </section>
 	);
 };
 
