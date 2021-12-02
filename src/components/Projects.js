86691c243a64d453c4569c720a9e1a61db53878c
Jason Toups <me@jasontoups.com>
2021-12-02 09:06:39 -0800

    fixes bottom footer background and map padding & margin

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,50 +14,29 @@ const Projects = () => {
       <div className="projects-container">
         <a
           className="project project-1"
-          href="https://jasontoups.github.io/rick-and-morty/"
+          href="https://geo-task-app.herokuapp.com/"
           target="_blank"
           rel="noreferrer"
-        > 
-          <div className="project-image-container">
-            <img
-            className="project-image"
-            src={RickAndMorty}
-            alt="rick and morty database"
-            />
-          </div>
-          <span className="project-description">
-            <h2>Rick & Morty</h2>
-            <h3>Database</h3>
-            <p>
-              This site uses the Rick and Morty API to render a database of characters, locations and episodes. 
-            </p>
-          </span>
-        </a>
-        <a
-          className="project project-2"
-          href="https://jasontoups.github.io/CrushIt"
-          target="_blank"
-          rel="noreferrer"
-        > 
+        >
           <div className="project-image-container">
             <img
               className="project-image"
-              src={CrushIt}
-              alt="crush it game"
-            />
+              src={GeoTask}
+              alt="geotask project"
+            />   
           </div>
           <span className="project-description">
             <span className="caption-content">
-              <h2>Crush It!!!</h2>
+              <h2>Geotask</h2>
               <p>
-                Mobile Block Crushing Game built with JS/JQuery/HTML5, with
-                only CSS assets. Styled after Candy Crush.
+                A productivity app with geolocation for todo lists. Built
+                with Google OAuth / Maps / Places API, NodeJS & MongoDB.
               </p>
             </span>
           </span>
         </a>
         <a
-          className="project project-3"
+          className="project project-2"
           href="https://forever-homes-app.herokuapp.com/"
           target="_blank"
           rel="noreferrer"
@@ -80,24 +59,45 @@ const Projects = () => {
           </span>
         </a>
         <a
-          className="project project-4"
-          href="https://geo-task-app.herokuapp.com/"
+          className="project project-3"
+          href="https://jasontoups.github.io/rick-and-morty/"
           target="_blank"
           rel="noreferrer"
-        >
+        > 
+          <div className="project-image-container">
+            <img
+            className="project-image"
+            src={RickAndMorty}
+            alt="rick and morty database"
+            />
+          </div>
+          <span className="project-description">
+            <h2>Rick & Morty</h2>
+            <h3>Database</h3>
+            <p>
+              This site uses the Rick and Morty API to render a database of characters, locations and episodes. 
+            </p>
+          </span>
+        </a>
+        <a
+          className="project project-4"
+          href="https://jasontoups.github.io/CrushIt"
+          target="_blank"
+          rel="noreferrer"
+        > 
           <div className="project-image-container">
             <img
               className="project-image"
-              src={GeoTask}
-              alt="geotask project"
-            />   
+              src={CrushIt}
+              alt="crush it game"
+            />
           </div>
           <span className="project-description">
             <span className="caption-content">
-              <h2>Geotask</h2>
+              <h2>Crush It!!!</h2>
               <p>
-                A productivity app with geolocation for todo lists. Built
-                with Google OAuth / Maps / Places API, NodeJS & MongoDB.
+                Mobile Block Crushing Game built with JS/JQuery/HTML5, with
+                only CSS assets. Styled after Candy Crush.
               </p>
             </span>
           </span>
