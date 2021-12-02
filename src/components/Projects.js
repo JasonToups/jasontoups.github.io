9f3029b86c8f904e0a1dd5ce8e6ca95f769267e2
Jason Toups <me@jasontoups.com>
2021-12-01 18:02:28 -0800

    adds the rick & morty svg for the app image

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import RickAndMorty from '../img/projects/rick-and-morty-portfolio.png';
+import RickAndMorty from '../img/projects/logo-RickAndMorty.svg';
 import CrushIt from '../img/projects/crush-it-logo.svg';
 import ForeverHomes from '../img/projects/foreverhomes-logo.svg';
 import GeoTask from '../img/projects/geotask-logo.svg';
