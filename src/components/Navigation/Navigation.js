c3d16de1a35ac3309036c3718378c47f84e95d98
Jason Toups <me@jasontoups.com>
2023-12-07 20:45:33 -0800

    updates graphic for nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ResumeIcon from '../../img/svg/resume.svg';
+import ProfileIcon from '../../img/svg/profile.svg';
 
 const Navigation = () => {
   const navigate = useNavigate();
+  const [currentIcon, setCurrentIcon] = useState('defaultIcon.png'); // Set default icon
 
   const toggleRoutes = () => {
     const currentPath = window.location.pathname;
 
     if (currentPath === '/') {
+      setCurrentIcon(ProfileIcon);
       navigate('/resume');
     } else {
+      setCurrentIcon(ResumeIcon);
       navigate('/');
     }
   };
 
   return (
-    <div>
-      <button onClick={toggleRoutes}>Toggle Routes</button>
+    <div className='navigation'>
+      <img src={currentIcon} onClick={toggleRoutes}></img>
     </div>
   );
 };
