133f036dbcdaabff1c42544decc0082233a1ca6e
Jason Toups <me@jasontoups.com>
2023-12-09 02:22:14 -0800

    [resume-component-refactor] error cleanup

--- src/components/Resume/Header/Header.js
+++ src/components/Resume/Header/Header.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
 const Header = ({ data }) => {
+  const { title, author, contact, experience } = data;
   return (
     <div className='resume-section'>
       <header id='title-block-header'>
-        <h1 className='title'>{data.title}</h1>
-        <p className='author'>{data.author}</p>
+        <h1 className='title'>{title}</h1>
+        <p className='author'>{author}</p>
       </header>
       <h5 id='contact'>
-        {data.contact.map((item, index) => (
-          <>
+        {contact.map((item, index) => (
+          <React.Fragment key={index}>
             <a href={item.link}>{item.display}</a>{' '}
             {index < data.contact.length - 1 ? ' . ' : ''}
-          </>
+          </React.Fragment>
         ))}
       </h5>
-      <h5 id='experience'>{data.experience}</h5>
+      <h5 id='experience'>{experience}</h5>
     </div>
   );
 };
