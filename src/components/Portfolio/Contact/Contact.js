ad7efcd485e19edaef503230ca1b2b4d29c51aef
Jason Toups <me@jasontoups.com>
2023-12-08 16:59:45 -0800

    removes unused import

--- src/components/Portfolio/Contact/Contact.js
+++ src/components/Portfolio/Contact/Contact.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFile } from '@fortawesome/free-solid-svg-icons';
-
 import {
   faLinkedinIn,
   faGithubAlt,
