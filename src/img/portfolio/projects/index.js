333a24c0651f79596d41d04bba86e5da78cd3a8a
Jason Toups <me@jasontoups.com>
2023-12-10 09:57:06 -0800

    [markdown-resume-app] added Markdown Resume to list of apps

--- src/img/portfolio/projects/index.js
+++ src/img/portfolio/projects/index.js
@@ -2,5 +2,6 @@ import RickAndMorty from './logo-RickAndMorty.svg';
 import CrushIt from './crush-it-logo.svg';
 import ReactTube from './reacttube-logo.svg';
 import ReactUnsplash from './reactunsplash-logo.svg';
+import MarkdownResume from './markdownresume-logo.svg';
 
-export { RickAndMorty, CrushIt, ReactTube, ReactUnsplash };
+export { RickAndMorty, CrushIt, ReactTube, ReactUnsplash, MarkdownResume };
