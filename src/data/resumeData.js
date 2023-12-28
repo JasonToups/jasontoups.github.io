f0f0cb40fc50e5e714b91ebc00bce6a30739222c
Jason Toups <me@jasontoups.com>
2023-12-28 14:02:04 -0800

    resume update

--- src/data/resumeData.js
+++ src/data/resumeData.js
@@ -163,9 +163,9 @@ export const workExperienceData = [
       'Engineered & Designed mobile marketing materials for the engineering, creative, and marketing teams.',
     jobTasks: [
       'Engineered HTML playable ads in Javascript, with Adobe Animate for clients.',
+      'Implemented Responsive Design solution that consistently generated lift across all existing playable ads, and became an engineering standard.',
       'Created Playable Ad Production Workflow, to design mockups and storyboards for ad approval, and CSS for engineers.',
       'Upgraded client assets with self-designed elements, that were approved by advertisers including Disney, Warner Brothers & King, for their Frozen, Harry Potter, and Candy Crush IPs.',
-      'Implemented Responsive Design solution that consistently generated lift across all existing playable ads, and became an engineering standard.',
     ],
     technologies: [
       'Javascript',
