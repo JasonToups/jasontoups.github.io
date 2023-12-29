9d889140a68bb78e04a5cd9a127ed52d278ece17
Jason Toups <me@jasontoups.com>
2023-12-28 19:04:20 -0800

    [title-card] added ampersand svg & attempted some animation

--- src/components/Portfolio/Header/Header.js
+++ src/components/Portfolio/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion/dist/framer-motion';
 
 const Header = ({ data }) => {
   const { image, imageAlt, title } = data;
@@ -6,7 +7,12 @@ const Header = ({ data }) => {
   return (
     <div className='app-header flex-container .flex-direction-column'>
       <div className='content-container'>
-        <img className='header-logo' alt={imageAlt} src={image} />
+        <motion.img
+          className='header-logo'
+          alt={imageAlt}
+          src={image}
+          whileHover={{ scale: 1.2 }}
+        />
         <h1>{title}</h1>
       </div>
     </div>
