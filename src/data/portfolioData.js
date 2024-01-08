a5b3aa3757bcfaef0a66d51a32eb3d6147689da0
Jason Toups <me@jasontoups.com>
2024-01-07 17:19:03 -0800

    [spline-animation] setting up for site redesign

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -69,7 +69,7 @@ export const aboutData = {
     "Thanks for stopping by. I'm a React engineer & designer, with over 15 years of software production experience; 7 years in Javascript, 12 years in QA and 3 years in UI/UX Design.",
     'My specialty is working with React Hooks, Redux & Thunk; Designing Vector Graphics & Wireframing',
     'I designed all of the vector graphics here in Affinity Designer, and styled my portfolio from scratch with Sass & Mixins',
-    'I can design, engineer, & test new applications.',
+    'I can engineer, design, & test new applications.',
     'I’ve been working in Agile software production environments for over a decade. When working in a team environment, my task-tracking, written communication & documentation are unmatched.',
     'If you are looking for a unicorn that can do just about anything in the digital realm, you’ve found your engineer. 🦄👨‍💻✨',
   ],
@@ -98,8 +98,8 @@ export const experienceData = {
       imageUrl: 'http://www.williams-sonomainc.com/',
       title: 'Williams Sonoma, Inc.',
       description: [
-        'Working on the Micro Front End team, transitioning Production pages from 6 brands, into a unified Vue project, with 6 brand configs.',
-        'Engineering architectural & styling updates to Vue components for all 6 supported brands, including; the Product Information pages for the Purchasing Component & Product Flags Component, Shop pages, & Content pages.',
+        'Worked on the Micro Front End team, transitioning Production pages from 6 brands, into a unified Vue project, with 6 brand configs.',
+        'Engineered architectural & styling updates to Vue components for all 6 supported brands, including; the Product Information pages for the Purchasing Component & Product Flags Component, Shop pages, & Content pages.',
         'Solved Local Environment Stability Issues across several teams with a self-directed Node & Shell project that automated repetitive engineering tasks, including; cloning 11 repos pointing to multiple remotes per brand, running 4-5 server commands, creating multiple feature branches across each brand with an interactive Shell script, updating local branches to the latest release code, and pruning the Docker container.',
         'Managing & Migrating pull requests from team-specific repos, to Bedrock & Release Management repos, including batch cherry-picked commits from my team to the upstream repos.',
         'Mentoring junior engineers on my team with paired-programming sessions and sharing helpful resources.',
@@ -112,10 +112,10 @@ export const experienceData = {
       title: 'Newlywords',
       description: [
         'NewlyWords is a React & Ruby memory book platform where hundreds of users per day can invite their friends & family to collaborate on a commemorative digital or physical book.',
-        'I engineered the Memory Book Project Dashboard in React with custom Hooks, where project owners view and edit pages submitted from their contributors, and view information about their book.',
-        'I built the Questionnaire as a the new memory book content type, providing project owners a place to ask their contributors questions to answer for their collaborative memory book content.',
-        'I developed a design guide with classNames and partials, from the product design system, to build stylesheets which implemented reusable classnames and styles.',
-        'I mentored a junior ui/ux designer by providing design advice to help him utilize Figma features more robustly, and to think about user experience from our user’s perspective, following a contextual flow of page elements.',
+        'Engineered the Memory Book Project Dashboard in React with custom Hooks, where project owners view and edit pages submitted from their contributors, and view information about their book.',
+        'Built the Questionnaire as a the new memory book content type, providing project owners a place to ask their contributors questions to answer for their collaborative memory book content.',
+        'Developed a design guide with classNames and partials, from the product design system, to build stylesheets which implemented reusable classnames and styles.',
+        'Mentored a junior ui/ux designer by providing design advice to help him utilize Figma features more robustly, and to think about user experience from our user’s perspective, following a contextual flow of page elements.',
       ],
     },
     {
@@ -154,7 +154,7 @@ export const projectsData = {
       projectNumber: '2',
       title: 'ReactTube',
       description:
-        'Search the YouTube API and return a short list of 5 videos, utilizing Primitive Hooks & Custom Hooks to fetch data.',
+        'Search the YouTube API and return a short list of 5 videos, utilizing Primitive & Custom Hooks to fetch data.',
     },
     {
       image: CrushIt,
