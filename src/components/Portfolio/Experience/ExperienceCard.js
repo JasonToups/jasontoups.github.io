65e0d2789fb7163b5deee70d1de19fbb0f5be4b0
Jason Toups <me@jasontoups.com>
2024-01-08 22:54:10 -0800

    [experience] updated all of the experience data before working on the template

--- src/components/Portfolio/Experience/ExperienceCard.js
+++ src/components/Portfolio/Experience/ExperienceCard.js
@@ -14,25 +14,47 @@ const ExperienceCard = ({ data }) => {
   } = data;
 
   return (
-    <div className='experience-card'>
-      <div className='experience-card--image-container'>
-        <a
-          className='experience-card--link'
-          href={imageUrl}
-          target='_blank'
-          rel='noreferrer'
-        >
-          <img className='experience-card--image' src={image} alt={imageAlt} />
-        </a>
-      </div>
-      <div className='experience-card--description'>
-        <h3>{title}</h3>
-
-        {description.map((item, index) => (
+    <main className='experience-card glass-background'>
+      <section className='company'>
+        <h3>{company.name}</h3>
+        <aside>{company.description}</aside>
+      </section>
+      <section className='description'>
+        {job.description.map((item, index) => (
           <p key={index}>{item}</p>
         ))}
-      </div>
-    </div>
+      </section>
+      <section className='image'>
+        <div className='experience-card--image-container'>
+          <a
+            className='experience-card--link'
+            href={company.url}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img
+              className='experience-card--image'
+              src={company.image.source}
+              alt={company.image.alt}
+            />
+          </a>
+        </div>
+      </section>
+      <section className='title'>
+        <h4>{job.title}</h4>
+        <aside>{job.duration}</aside>
+      </section>
+      <section className='technologies'>
+        {job.tech.map((item, index) => (
+          <img
+            key={index}
+            className='experience-card--technology'
+            src={item.source}
+            alt={item.alt}
+          />
+        ))}
+      </section>
+    </main>
   );
 };
 
--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -4,21 +4,31 @@ import PortfolioLogo from '../img/portfolio/hero/hero-logo.svg';
 import ProfilePic from '../img/portfolio/about/profile-pic-jason-toups-masked.png';
 // Imports for the Experience Section
 import {
+  Bill,
+  Chartboost,
   Huoa,
+  LifeStreet,
   NewlyWords,
   WilliamsSonoma,
-  Bill,
 } from '../img/portfolio/experience/index.js';
 // Technologies Imports
 import {
   AEM,
+  An,
   Angular,
+  Css,
+  Javascript,
   Jest,
   JQuery,
+  Metamarkets,
   RxJS,
   React,
+  Ruby,
   Sass,
+  Sketch,
   TypeScript,
+  Vue,
+  Wordpress,
 } from '../img/portfolio/technologies/index.js';
 // Imports for the Projects Section
 import { RickAndMorty, CrushIt, ReactTube } from '../img/portfolio/index.js';
@@ -53,72 +63,160 @@ export const experienceData = {
   header: 'Experience',
   workExperience: [
     {
-      title: 'Bill',
-      image: Bill,
-      imageAlt: 'Bill.com',
-      imageUrl: 'http://www.bill.com/',
-      imageTemp: {
-        source: Bill,
-        alt: 'Bill.com',
-      },
       company: {
         name: 'Bill',
         description: 'Financial Operations Platform',
         url: 'http://www.bill.com/',
+        image: {
+          source: Bill,
+          alt: 'Bill.com',
+        },
       },
       job: {
-        title: 'Sr. Software Engineer',
-        Duration: 'Jul 2022 - Dec 2023',
+        title: 'Sr. Frontend Engineer',
+        duration: 'Jul 2022 - Dec 2023',
+        description: [
+          'Worked with Angular & RxJS on the revenue-generating International Payments Team.',
+          'We retrofitted existing Domestic payment flows, and added International Payment functionality to them.',
+        ],
+        tech: [
+          { source: Angular, alt: 'angular' },
+          { source: TypeScript, alt: 'typescript' },
+          { source: RxJS, alt: 'rxjs' },
+          { source: Sass, alt: 'sass' },
+          { source: Jest, alt: 'jest' },
+        ],
       },
-      description: [
-        'Worked with Angular & RxJS on the revenue-generating International Payments Team. We retrofitted existing Domestic payment flows, and added International Payment functionality to them.',
-        'Upgraded Domestic payment flows to include International options, enabling users to make International Payments with real-time exchange rates.',
-        'Worked seamlessly across teams, collaborating with code owners to implement new features and address bugs during the integration of International Payment functionality.',
-        'Developed custom package.json scripts to streamline repetitive engineering tasks, optimizing team productivity and reducing manual effort.',
-        'Created Markdown-based training documentation on Confluence, facilitating smooth onboarding for new engineers and standardizing processes, including creating the Merge Request templates for Releases & Feature/Bug tickets.',
-        'This Bill.com experience showcases my ability to deliver impactful solutions, contribute collaboratively, and optimize development processes in a dynamic environment.',
-      ],
-      tech: [Angular, TypeScript, RxJS, Sass, Jest],
     },
     {
-      image: WilliamsSonoma,
-      imageAlt: 'Williams Sonoma, Inc.',
-      imageUrl: 'http://www.williams-sonomainc.com/',
-      title: 'Williams Sonoma, Inc.',
-      description: [
-        'Worked on the Micro Front End team, transitioning Production pages from 6 brands, into a unified Vue project, with 6 brand configs.',
-        'Engineered architectural & styling updates to Vue components for all 6 supported brands, including; the Product Information pages for the Purchasing Component & Product Flags Component, Shop pages, & Content pages.',
-        'Solved Local Environment Stability Issues across several teams with a self-directed Node & Shell project that automated repetitive engineering tasks, including; cloning 11 repos pointing to multiple remotes per brand, running 4-5 server commands, creating multiple feature branches across each brand with an interactive Shell script, updating local branches to the latest release code, and pruning the Docker container.',
-        'Managing & Migrating pull requests from team-specific repos, to Bedrock & Release Management repos, including batch cherry-picked commits from my team to the upstream repos.',
-        'Mentoring junior engineers on my team with paired-programming sessions and sharing helpful resources.',
-      ],
+      company: {
+        name: 'Williams Sonoma, Inc.',
+        description:
+          '6 eCommerce sites, including Williams Sonoma, West Elm, & Pottery Barn',
+        url: 'http://www.williams-sonomainc.com/',
+        image: {
+          source: WilliamsSonoma,
+          alt: 'Williams Sonoma Incorporated',
+        },
+      },
+      job: {
+        title: 'Frontend Engineer',
+        duration: 'Apr 2021 - June 2022',
+        description: [
+          'Worked with the Micro Front End team, transitioning Production pages from 6 brands, into a unified Vue project.',
+          'Updated Vue Components including; Product Information pages for the Purchasing Component & Product Flags, Shop & Content pages.',
+        ],
+        tech: [
+          { source: Vue, alt: 'vue' },
+          { source: Javascript, alt: 'javascript' },
+          { source: AEM, alt: 'adobe experience manager' },
+          { source: Sass, alt: 'sass' },
+          { source: Jest, alt: 'jest' },
+        ],
+      },
     },
     {
-      image: NewlyWords,
-      imageAlt: 'NewlyWords',
-      imageUrl: 'https://www.newlywords.com/',
-      title: 'Newlywords',
-      description: [
-        'NewlyWords is a React & Ruby memory book platform where hundreds of users per day can invite their friends & family to collaborate on a commemorative digital or physical book.',
-        'Engineered the Memory Book Project Dashboard in React with custom Hooks, where project owners view and edit pages submitted from their contributors, and view information about their book.',
-        'Built the Questionnaire as a the new memory book content type, providing project owners a place to ask their contributors questions to answer for their collaborative memory book content.',
-        'Developed a design guide with classNames and partials, from the product design system, to build stylesheets which implemented reusable classnames and styles.',
-        'Mentored a junior ui/ux designer by providing design advice to help him utilize Figma features more robustly, and to think about user experience from our user’s perspective, following a contextual flow of page elements.',
-      ],
+      company: {
+        name: 'NewlyWords',
+        description: 'Collaborative Memory Book creation & publishing',
+        url: 'https://www.newlywords.com/',
+        image: {
+          source: NewlyWords,
+          alt: 'Newly Words',
+        },
+      },
+      job: {
+        title: 'Frontend Engineer',
+        duration: 'Oct 2020 - Apr 2021',
+        description: [
+          'Engineered the Memory Book Project Dashboard in React with custom Hooks.',
+          'Built the Questionnaire as a new memory book content type.',
+          'Developed a design guide.',
+          'Mentored a junior ui/ux designer.',
+        ],
+        tech: [
+          { source: React, alt: 'react' },
+          { source: Javascript, alt: 'javascript' },
+          { source: Ruby, alt: 'ruby' },
+          { source: Sass, alt: 'sass' },
+        ],
+      },
     },
     {
-      image: Huoa,
-      imageAlt: 'Hawaii United Okinawa Association',
-      imageUrl:
-        'https://github.com/Hawaian-United-Okinawa-Association/huoa-react',
-      title: 'Hawaii United Okinawa Association',
-      description: [
-        "Rebuilt original static site to be in the JAMstack, improving speed, page weight, and security of the older version of the client's website. Built with: React, React-Redux, Hooks, with a Wordpress backend and mobile support.",
-        'Contributed to component library, creating dynamic components rendering images and content served up by the WordPress API.',
-        'Engineered the reusable React Container components to control all layouts (Web, Tablet, Mobile) in CSS Grid and Sass, along with reusable Card components that are used site-wide.',
-        'Added Docker to the project to keep all of our dev environments in sync with the latest dependencies.',
-        'Onboarded new engineers by writing onboarding documentation in markdown & included it in the repo.',
-      ],
+      company: {
+        name: 'Hawaii United Okinawa Association',
+        description: 'Cultural exchange between Hawaii & Japan',
+        url: 'https://github.com/Hawaian-United-Okinawa-Association/huoa-react',
+        image: {
+          source: Huoa,
+          alt: 'Hawaii United Okinawa Association',
+        },
+      },
+      job: {
+        title: 'Frontend Engineer',
+        duration: 'Apr 2020 - Oct 2020',
+        description: [
+          'Refactored existing static HUOA site to a dynamic React site, using Wordpress on the backend.',
+          'Built reusable components that were used across the site.',
+          'Engineered custom Hooks to handle State between multiple components.',
+        ],
+        tech: [
+          { source: React, alt: 'react' },
+          { source: Javascript, alt: 'javascript' },
+          { source: Wordpress, alt: 'wordpress' },
+          { source: Sass, alt: 'sass' },
+        ],
+      },
+    },
+    {
+      company: {
+        name: 'LifeStreet',
+        description: 'Programmatic Mobile Marketing Platform',
+        url: 'https://lifestreet.com/',
+        image: {
+          source: LifeStreet,
+          alt: 'life street',
+        },
+      },
+      job: {
+        title: 'Sr. Product Designer',
+        duration: ' Sept 2017 - Oct 2019',
+        description: [
+          'Engineered & Designed mobile marketing materials.',
+          'Built HTML playable ads in Javascript, with Adobe Animate for clients.',
+          'Designed UI/UX storyboards for playable ads in Sketch.',
+        ],
+        tech: [
+          { source: An, alt: 'adobe animate' },
+          { source: Javascript, alt: 'javascript' },
+          { source: Sketch, alt: 'sketch' },
+          { source: Css, alt: 'cascading style sheets' },
+        ],
+      },
+    },
+    {
+      company: {
+        name: 'Chartboost',
+        description: 'Programmatic Mobile Advertising Network',
+        url: 'https://www.chartboost.com/',
+        image: {
+          source: Chartboost,
+          alt: 'chart boost',
+        },
+      },
+      job: {
+        title: 'Product Designer',
+        duration: 'Feb 2017 - Aug 2017',
+        description: [
+          'Designed UI/UX storyboards for playable ads in Sketch.',
+          'Scaled the production of playables 170% in the first 2 months.',
+          'Iterated existing ads with A/B testing, and reported results from Metamarkets data to all stakeholders.',
+        ],
+        tech: [
+          { source: Sketch, alt: 'sketch' },
+          { source: Metamarkets, alt: 'meta markets' },
+        ],
+      },
     },
   ],
 };
