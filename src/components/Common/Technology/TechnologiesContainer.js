481565f0178eba85aeef8eba1e06fef7c5fa3266
Jason Toups <me@jasontoups.com>
2024-01-09 23:50:31 -0800

    [browser-support] last updates for fixes

--- src/components/Common/Technology/TechnologiesContainer.js
+++ src/components/Common/Technology/TechnologiesContainer.js
@@ -5,15 +5,17 @@ const TechnologiesContainer = ({ data }) => {
   // console.log(data);
 
   return (
-    <div className='technologies-container glass-foreground'>
-      {data.map((data, index) => (
-        <img
-          key={index}
-          className='technology'
-          src={data.source}
-          alt={data.alt}
-        />
-      ))}
+    <div className='technologies-container'>
+      <div className='technologies-list glass-foreground'>
+        {data.map((data, index) => (
+          <img
+            key={index}
+            className='technology'
+            src={data.source}
+            alt={data.alt}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Portfolio/Projects/ProjectCard.js
+++ src/components/Portfolio/Projects/ProjectCard.js
@@ -3,19 +3,28 @@ import { TechnologiesContainer } from '../../Common/Technology/index';
 
 const ProjectCard = ({ data }) => {
   return (
-    <a
+    <div
       className={`project project-${data.projectNumber} glass-background border-radius`}
-      href={data.imageUrl}
-      target='_blank'
-      rel='noreferrer'
     >
-      <div className='project-image-container'>
+      <a
+        className='project-image-container'
+        href={data.imageUrl}
+        target='_blank'
+        rel='noreferrer'
+      >
         <img className='project-image' src={data.image} alt={data.imageAlt} />
-      </div>
-      <h3 className='project-title'>{data.title}</h3>
+      </a>
+      <a
+        className='project-title'
+        href={data.imageUrl}
+        target='_blank'
+        rel='noreferrer'
+      >
+        <h3>{data.title}</h3>
+      </a>
       <p className='project-description'>{data.description}</p>
       <TechnologiesContainer data={data.technologies} />
-    </a>
+    </div>
   );
 };
 
