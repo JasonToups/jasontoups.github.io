e544a3507df12c3246f91d90e63f3afdb3df4e4e
Jason Toups <me@jasontoups.com>
2024-01-09 20:39:21 -0800

    [contact-section] final touches before deployment

--- src/components/Portfolio/Footer/Footer.js
+++ src/components/Portfolio/Footer/Footer.js
@@ -8,7 +8,25 @@ const Contact = ({ data }) => {
   }, []);
 
   return (
-    <footer className='footer'>Copyright &copy; Jason Toups {date}</footer>
+    <section className='footer'>
+      <footer>
+        <p>All assets lovingly crafted by hand, by yours truly.</p>
+        <p>No AI assistance was used in designing this site.</p>
+        <p>
+          Checkout my{' '}
+          <a href='https://github.com/JasonToups/jasontoups.github.io'>
+            <code>source code</code>
+          </a>
+          , or my{' '}
+          <a href='https://github.com/users/JasonToups/projects/2'>
+            <code>github project</code>
+          </a>
+          ,
+          <br /> where I planned the design, development & deployment.
+        </p>
+        <aside>Copyright &copy; Jason Toups {date}</aside>
+      </footer>
+    </section>
   );
 };
 
