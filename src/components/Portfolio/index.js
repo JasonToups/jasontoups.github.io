58f98a9929238f4bc7248d600738f64fed092636
Jason Toups <me@jasontoups.com>
2024-01-19 14:12:45 -0800

    [development] removed unused Map Component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,7 +3,6 @@ import Connect from './Connect/Connect';
 import Footer from './Footer/Footer';
 import Experience from './Experience/Experience';
 import Hero from './Hero/Hero';
-import Map from './Map/Map';
 import Projects from './Projects/Projects';
 
-export { About, Connect, Experience, Footer, Hero, Map, Projects };
+export { About, Connect, Experience, Footer, Hero, Projects };
