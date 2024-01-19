0dff297d1f7e325e906a58cccd4f256a9434c3d9
Jason Toups <me@jasontoups.com>
2024-01-18 22:53:25 -0800

    [development] aligns content in Connect

--- src/components/Portfolio/Connect/Connect.js
+++ src/components/Portfolio/Connect/Connect.js
@@ -15,51 +15,63 @@ const Connect = ({ data }) => {
 
   return (
     <section className='connect'>
-      <img
-        className='connect-image'
-        src={data.header.source}
-        alt={data.header.alt}
-      />
-      <div className='connect-card-background glass-background'>
+      <div className='connect-header'>
         <img
-          className='profile-pic'
-          src={data.profilePic.source}
-          alt={data.profilePic.alt}
+          className='connect-image'
+          src={data.header.source}
+          alt={data.header.alt}
         />
-        <div className='connect-card-foreground'>
-          <div className='connect-card'>
-            <div className='connect-icons-container'>
-              {data.icons.map((contact, index) => (
-                <a
-                  key={index}
-                  href={contact.url}
-                  target='_blank'
-                  rel='noreferrer'
-                  className={contact.class}
-                >
-                  <img
-                    className='connect-icon'
-                    src={contact.source}
-                    alt={contact.alt}
-                  />
-                </a>
-              ))}
-              <a
-                href='mailto:me@jasontoups.com'
-                target='_blank'
-                rel='noreferrer'
-                className='email'
-              >
-                <FontAwesomeIcon
-                  className='connect-icon fa-7x'
-                  icon={faEnvelope}
-                  alt='email jason toups'
-                />
-              </a>
+      </div>
+      <div className='connect-container'>
+        <div className='connect-background'>
+          <img
+            src={data.background.circle.source}
+            alt={data.background.circle.alt}
+          />
+        </div>
+        <div className='connect-content'>
+          <div className='connect-card-background glass-background'>
+            <img
+              className='profile-pic'
+              src={data.profilePic.source}
+              alt={data.profilePic.alt}
+            />
+            <div className='connect-card-foreground'>
+              <div className='connect-card'>
+                <div className='connect-icons-container'>
+                  {data.icons.map((contact, index) => (
+                    <a
+                      key={index}
+                      href={contact.url}
+                      target='_blank'
+                      rel='noreferrer'
+                      className={contact.class}
+                    >
+                      <img
+                        className='connect-icon'
+                        src={contact.source}
+                        alt={contact.alt}
+                      />
+                    </a>
+                  ))}
+                  <a
+                    href='mailto:me@jasontoups.com'
+                    target='_blank'
+                    rel='noreferrer'
+                    className='email'
+                  >
+                    <FontAwesomeIcon
+                      className='connect-icon fa-7x'
+                      icon={faEnvelope}
+                      alt='email jason toups'
+                    />
+                  </a>
+                </div>
+                <button type={data.button.type} onClick={handleResumeClick}>
+                  {data.button.text}
+                </button>
+              </div>
             </div>
-            <button type={data.button.type} onClick={handleResumeClick}>
-              {data.button.text}
-            </button>
           </div>
         </div>
       </div>
