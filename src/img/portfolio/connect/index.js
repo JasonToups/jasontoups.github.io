0282b762b59286c93206e79fbfa0edbd4babe4ef
Jason Toups <me@jasontoups.com>
2024-01-23 16:55:11 -0800

    [class-refactor] added data folders for common & connect

--- src/img/portfolio/connect/index.js
+++ src/img/portfolio/connect/index.js
@@ -4,6 +4,12 @@ import ConnectCardBackground from './connect-card-background.svg';
 import LinkedIn from './linkedin.svg';
 import GitHub from './github.svg';
 import Email from './email.svg';
+import ConnectGraphicSource from './connect.png';
+import ConnectCircleSource from './connect-circle.svg';
+import ConnectCardBackgroundSource from './connect-card-background.svg';
+import LinkedInSource from './linkedin.svg';
+import GitHubSource from './github.svg';
+import EmailSource from './email.svg';
 
 export {
   ConnectGraphic,
@@ -12,4 +18,10 @@ export {
   LinkedIn,
   GitHub,
   Email,
+  ConnectGraphicSource,
+  ConnectCircleSource,
+  ConnectCardBackgroundSource,
+  LinkedInSource,
+  GitHubSource,
+  EmailSource,
 };
